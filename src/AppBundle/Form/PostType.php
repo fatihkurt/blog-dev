cc6bccf3212a4cb0eea312c1c8a816c3bac8e675
Fatih Kurt <fatih.kurt@liderlervadisi.com>
2016-04-06 12:02:23 +0300

    Symfony generator ile post mvc çıkartıldı

---
+++ src/AppBundle/Form/PostType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('summary')
+            ->add('content')
+            ->add('authorEmail')
+            ->add('publishedAt', 'datetime')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Post'
+        ));
+    }
+}
